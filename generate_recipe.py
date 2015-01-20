152466da76ba88d8bf0648e64669398c68efc913
schollz <zack.scholl@gmail.com>
2015-01-19 19:49:58 -0500

    Added durations

--- generate_recipe.py
+++ generate_recipe.py
@@ -1,6 +1,9 @@
 import yaml
+from pint import UnitRegistry
 
-def start_print_dict(item,fullDict,printOut):
+ureg = UnitRegistry()
+
+def start_print_dict(item,fullDict,printOut,stats):
 	if printOut:
 		return 'To make ' + fullDict[item]['amount'] + ' ' + item + ':\n\n' + print_dict(fullDict[item],'',fullDict)
 	else:
@@ -20,21 +23,28 @@ def print_dict(dictionary, string, fullDict):
 					string = string + " " + e['amount'] + ' ' + e['name'] + " "
 					num = num + 1
 					if e['name'] in fullDict.keys():
-						string = start_print_dict(e['name'],fullDict,False) + "\n" + string
+						string = start_print_dict(e['name'],fullDict,False,stats) + "\n" + string
 				string = string + "\n"
 		if 'mix' == key:
 			string = string + "Mix them together\n"
+			stats['time'] = stats['time'] + 2*ureg.minute
 		if 'cook' == key:
 			string = string + value['type'] + " at " + value['heat'] + " for " + value['time'] + "\n"
+			stats['time'] = stats['time'] + (int(value['time'].split()[0])*ureg.parse_expression(value['time'].split()[1]))
 		if 'set' == key:
 			string = string + "Set for " + value['time'] + "\n"
+			stats['time'] = stats['time'] + (int(value['time'].split()[0])*ureg.parse_expression(value['time'].split()[1]))
 		if 'cut' == key:
 			string = string + value['type'] + " into " + value['pieces']  + "\n"
+			stats['time'] = stats['time'] + 3*ureg.minute
 	return string
 		
 			
 stream = open('recipes.yaml','r')
 data = yaml.load(stream, yaml.SafeLoader)
-print start_print_dict('grilled cheese sandwich',data,True)
+stats = {}
+stats['time']=0*ureg.minute
+print start_print_dict('grilled cheese sandwich',data,True,stats)
+print stats
 
 		
