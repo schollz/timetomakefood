c7bce993faa065938362a1a25b7f1c4bfca86fd3
Zack Scholl <zack.scholl@gmail.com>
2017-01-18 21:17:55 -0500

    Added reaction header

--- run.go
+++ run.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/BurntSushi/toml"
@@ -36,71 +39,68 @@ func (d *Duration) UnmarshalText(text []byte) error {
 
 func main() {
 	fmt.Println("vim-go")
-	var rs reactions
-	bData, _ := ioutil.ReadFile("./data/reactions.toml")
-	if _, err := toml.Decode(string(bData), &rs); err != nil {
+
+	// http://stackoverflow.com/questions/6608873/file-system-scanning-in-golang
+	searchDir := "./data/"
+
+	fileList := []string{}
+	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		if strings.Contains(path, ".toml") {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	if err != nil {
 		panic(err)
 	}
-	bJson, _ := json.MarshalIndent(rs, "", "  ")
-	fmt.Println(string(bJson))
-	// // http://stackoverflow.com/questions/6608873/file-system-scanning-in-golang
-	// searchDir := "./data/"
 
-	// fileList := []string{}
-	// err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
-	// 	if strings.Contains(path, ".toml") {
-	// 		fileList = append(fileList, path)
-	// 	}
-	// 	return nil
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
+	var rs reactions
 
-	// reactions := make([]Reaction, len(fileList))
+	for _, file := range fileList {
+		fmt.Println(file)
+		var rsTemp reactions
+		bData, _ := ioutil.ReadFile(file)
+		if _, err := toml.Decode(string(bData), &rsTemp); err != nil {
+			panic(err)
+		}
+		rs.Reaction = append(rs.Reaction, rsTemp.Reaction...)
+		fmt.Println(rsTemp.Reaction[0].Products)
+	}
 
-	// for i, file := range fileList {
-	// 	fmt.Println(file)
-	// 	bData, _ := ioutil.ReadFile(file)
-	// 	if _, err := toml.Decode(string(bData), &reactions[i]); err != nil {
-	// 		panic(err)
-	// 	}
-	// }
+	hasReactants := make(map[string]bool)
+	for _, reaction := range rs.Reaction {
+		for _, reactant := range reaction.Reactants {
+			hasReactants[reactant] = false
+		}
+	}
+	for _, reaction := range rs.Reaction {
+		for _, product := range reaction.Products {
+			if _, ok := hasReactants[product]; ok {
+				hasReactants[product] = true
+			}
+		}
+	}
 
-	// hasReactants := make(map[string]bool)
-	// for _, reaction := range reactions {
-	// 	for _, reactant := range reaction.Reactants {
-	// 		hasReactants[reactant] = false
-	// 	}
-	// }
-	// for _, reaction := range reactions {
-	// 	for _, product := range reaction.Products {
-	// 		if _, ok := hasReactants[product]; ok {
-	// 			hasReactants[product] = true
-	// 		}
-	// 	}
-	// }
+	fmt.Println("\nNo reactions exist to create the following products:")
+	for reactant := range hasReactants {
+		if !hasReactants[reactant] {
+			fmt.Println(reactant)
+		}
+	}
 
-	// fmt.Println("\nNo reactions exist to create the following products:")
-	// for reactant := range hasReactants {
-	// 	if !hasReactants[reactant] {
-	// 		fmt.Println(reactant)
-	// 	}
-	// }
+	// Produce graphviz dot
+	graphviz := "digraph G {\n"
+	for _, reaction := range rs.Reaction {
+		graphviz += "\t{" + strings.Join(reaction.Reactants, " ") + "} -> {" + strings.Join(reaction.Products, " ") + "};\n"
+	}
+	graphviz += "}"
+	fmt.Println(graphviz)
 
-	// // Produce graphviz dot
-	// graphviz := "digraph G {\n"
-	// for _, reaction := range reactions {
-	// 	graphviz += "\t{" + strings.Join(reaction.Reactants, " ") + "} -> {" + strings.Join(reaction.Products, " ") + "};\n"
-	// }
-	// graphviz += "}"
-	// fmt.Println(graphviz)
-
-	// ioutil.WriteFile("graphviz.dot", []byte(graphviz), 0755)
-	// png, err := exec.Command("dot", "-Tpng", "graphviz.dot").Output()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// ioutil.WriteFile("graph.png", png, 0755)
-	// os.Remove("graphviz.dot")
+	ioutil.WriteFile("graphviz.dot", []byte(graphviz), 0755)
+	png, err := exec.Command("dot", "-Tpng", "graphviz.dot").Output()
+	if err != nil {
+		panic(err)
+	}
+	ioutil.WriteFile("graph.png", png, 0755)
+	os.Remove("graphviz.dot")
 }
