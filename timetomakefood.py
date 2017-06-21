7a809e3219781e5ed7954a24f47b2611ee046c9f
Zack Scholl <zack.scholl@gmail.com>
2017-06-21 15:52:49 -0600

    Add cookies

--- timetomakefood.py
+++ timetomakefood.py
@@ -1,15 +1,18 @@
+import random 
+
 from flask import Flask
 from flask import render_template, request, redirect
 app = Flask(__name__)
 
 from lib.recipes import *
 
+CURRENT_RECIPES = ['grilled cheese sandwich','cookies']
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello(path):
     if path == "":
-        return redirect("/grilled-cheese-sandwich/", code=302)
+        return redirect("/%s/" % random.choice(CURRENT_RECIPES).replace(' ','-'), code=302)
     if path[-1] != "/":
         return redirect(path + "/", code=302)
     ingredients = path.replace('-', ' ').split("/")
@@ -18,8 +21,10 @@ def hello(path):
     if len(ingredients) > 2:
         other_ingredients = ingredients[1:-1]
     n = RecipeNetwork()
-    return render_template('main.html', recipe=n.generate_recipe(main_ingredient, other_ingredients), graphviz=n.generate_graphviz(ingredients))
+    other_recipes = CURRENT_RECIPES[:]
+    other_recipes.remove(main_ingredient)
+    return render_template('main.html', recipe=n.generate_recipe(main_ingredient, other_ingredients), graphviz=n.generate_graphviz(ingredients), other_recipes=other_recipes)
 
-
-from waitress import serve
-serve(app, listen='*:8082')
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, listen='*:8082')
