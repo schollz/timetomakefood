1a9c72b64bf1e1da18b0d5ff9540dbe5f03a3b32
Zack Scholl <zack.scholl@gmail.com>
2017-06-29 09:34:48 -0600

    Add refried beans

--- timetomakefood.py
+++ timetomakefood.py
@@ -16,7 +16,7 @@ logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:
     level=logging.DEBUG)
 logger = logging.getLogger('timetomakefood')
 
-CURRENT_RECIPES = ['tortilla', 'grilled cheese sandwich','cookies', 'noodles', 'white sauce','loaf of bread','eggs benedict','english muffin', 'pancakes', 'mustard', 'mayonnaise']
+CURRENT_RECIPES = ['tortilla', 'refried beans', 'grilled cheese sandwich','cookies', 'noodles', 'white sauce','loaf of bread','eggs benedict','english muffin', 'pancakes', 'mustard', 'mayonnaise']
 
 
 try:
